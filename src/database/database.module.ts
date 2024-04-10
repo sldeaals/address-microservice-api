3d01b71532ac95cf41d411a89469deac7080d3a3
sldeaals <danielariasaguero@gmail.com>
2024-04-10 17:33:58 -0400

    fix: database config file name

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { mongoConfig } from './mongo.config';
+import { mongoConfig } from './database.config';
 
 @Module({
   imports: [
