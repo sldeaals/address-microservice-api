202be17f0abea6b79e0882e966703752da2fb08f
Daniel Arias <64913142+sldeaals@users.noreply.github.com>
2024-04-11 21:08:09 -0400

    [AMA-007] - Add swagger routes and schemas documentation (#8)
    * feat: add swagger configuration
    
    * feat: add swagger to controllers

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+function useSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('Address Microservice API')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  useSwagger(app);
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
