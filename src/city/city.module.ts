d28a865eb1ae845dffc19dff23914d036ce7e13b
Daniel Arias <64913142+sldeaals@users.noreply.github.com>
2024-04-17 17:57:51 -0400

    [AMA-010] - Standardise api response (#11)
    * fix: city module missing dependencies
    
    * feat: add utils module
    
    * feat: add global exception filter and response interceptor

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -4,11 +4,13 @@ import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { CityModel, CitySchema } from './city.entity';
 import { StateSchema } from '../state/state.entity';
+import { DistrictSchema } from '../district/district.entity';
 
 @Module({
   imports: [MongooseModule.forFeature([
     { name: 'City', schema: CitySchema },
     { name: 'State', schema: StateSchema },
+    { name: 'District', schema: DistrictSchema },
   ])],
   controllers: [CityController],
   providers: [CityService, CityModel],
