8c30c3f3e8d80b260e1b8d90d011b0b3031229bf
sldeaals <danielariasaguero@gmail.com>
2024-04-21 17:03:18 -0400

    feat: create pagination util

---
+++ src/utils/pagination.util.ts
@@ -0,0 +1,31 @@
+export interface PaginationOptions {
+  page: number;
+  limit: number;
+}
+
+export interface PaginationResult<T> {
+  data: T[];
+  totalCount: number;
+  totalPages: number;
+  currentPage: number;
+}
+
+export function paginate<T>(
+  data: T[],
+  options: PaginationOptions,
+): PaginationResult<T> {
+  const { page, limit } = options;
+  const totalCount = data.length;
+  const totalPages = Math.ceil(totalCount / limit);
+  const currentPage = Math.min(page, totalPages);
+  const startIndex = (currentPage - 1) * limit;
+  const endIndex = startIndex + limit;
+  const paginatedData = data.slice(startIndex, endIndex);
+
+  return {
+    data: paginatedData,
+    totalCount,
+    totalPages,
+    currentPage,
+  };
+}
