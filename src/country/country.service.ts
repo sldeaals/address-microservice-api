46587c80515278ec1ad57142c1cf5fa18f7448cb
sldeaals <danielariasaguero@gmail.com>
2024-04-22 13:45:56 -0400

    update: apply generic pagination function to countries

--- src/country/country.service.ts
+++ src/country/country.service.ts
@@ -1,9 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CountryDocument } from './country.entity';
 import { CreateCountryDto, UpdateCountryDto, CountryFilterDto } from './country.dto';
-import { PaginationOptions, PaginationResult } from '../utils/pagination.util';
+import { PaginationOptions, PaginationResult, paginateSearch } from '../utils/pagination.util';
 
 @Injectable()
 export class CountryService {
@@ -39,38 +39,25 @@ export class CountryService {
   }
 
   async searchPaginated(options: PaginationOptions, filters: CountryFilterDto): Promise<PaginationResult<CountryDocument[]>> {
-    let nameRegex: RegExp;
-    const { page = 1, limit = 10 } = options;
-
-    const queryBuilder = this.countryModel.find();
-
-    if (filters?.name) {
-      nameRegex = new RegExp(filters.name, 'i');
-      queryBuilder.where('name.common').regex(nameRegex);
-    }
-    if (filters?.cca2) {
-      queryBuilder.where('cca2').equals(filters.cca2);
-    }
-    if (filters?.cca3) {
-      queryBuilder.where('cca3').equals(filters.cca3);
-    }
-
-    const totalCount = await this.countryModel.countDocuments(queryBuilder.getFilter());
-    const totalPages = Math.ceil(totalCount / limit);
-    const currentPage = totalCount ? Math.min(page, totalPages) : page;
-    const skip = totalCount ? ((currentPage - 1) * limit) : ((page - 1) * limit);
-
-    const data = await queryBuilder.skip(skip).limit(limit).exec();
-
-    if (!data || data.length === 0) {
-      throw new NotFoundException('No states found');
-    }
-
-    return {
-      data,
-      totalCount,
-      totalPages,
-      currentPage,
-    };
+    return paginateSearch<CountryDocument>(
+      this.countryModel,
+      options,
+      (queryBuilder, filters) => {
+        if (filters?.name) {
+          queryBuilder.or([
+            { 'name.common': { $regex: new RegExp(filters.name, 'i') } },
+            { 'name.official': { $regex: new RegExp(filters.name, 'i') } },
+          ]);
+        }
+        if (filters?.cca2) {
+          queryBuilder.where('cca2').equals(filters.cca2);
+        }
+        if (filters?.cca3) {
+          queryBuilder.where('cca3').equals(filters.cca3);
+        }
+        return queryBuilder;
+      },
+      filters
+    );
   }
 }
