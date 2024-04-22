84d53d76293fb0025ad1fbfabeb6a7c298809408
sldeaals <danielariasaguero@gmail.com>
2024-04-22 15:25:43 -0400

    fix: api response and exception filter file naming

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ApiResponseUtil } from './api-response.util';
-import { ExceptionsFilter } from './exception-filter.util';
+import { ApiResponseUtil } from './api.response.util';
+import { ExceptionsFilter } from './exception.filter.util';
 
 @Module({
   providers: [ApiResponseUtil, ExceptionsFilter],
