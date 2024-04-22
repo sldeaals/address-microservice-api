5a55b3733e9e59e12116c60a2217cedca26cf205
sldeaals <danielariasaguero@gmail.com>
2024-04-22 13:49:25 -0400

    update: apply generic pagination function to cities

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,11 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CityDocument } from './city.entity';
 import { StateDocument } from '../state/state.entity';
 import { DistrictDocument } from '../district/district.entity';
 import { CreateCityDto, UpdateCityDto, CityFilterDto } from './city.dto';
-import { PaginationOptions, PaginationResult } from '../utils/pagination.util';
+import { PaginationOptions, PaginationResult, paginateSearch } from '../utils/pagination.util';
 
 @Injectable()
 export class CityService {
@@ -42,41 +42,25 @@ export class CityService {
       .exec();
   }
 
-  async searchPaginated(options: PaginationOptions, filters: CityFilterDto): Promise<PaginationResult<CityDocument[]>> {
-    let nameRegex: RegExp;
-    const { page = 1, limit = 10 } = options;
-    
-    const queryBuilder = this.cityModel.find();
-
-    if (filters?.name) {
-      nameRegex = new RegExp(filters.name, 'i');
-      queryBuilder.where('name').regex(nameRegex);
-    }
-    if (filters?.countryCode) {
-      queryBuilder.where('countryCode').equals(filters.countryCode);
-    }
-
-    const totalCount = await this.cityModel.countDocuments(queryBuilder.getFilter());
-    const totalPages = Math.ceil(totalCount / limit);
-    const currentPage = totalCount ? Math.min(page, totalPages) : page;
-    const skip = totalCount ? ((currentPage - 1) * limit) : ((page - 1) * limit);
-
-    const data = await queryBuilder.skip(skip).limit(limit).exec();
-
-    if (!data || data.length === 0) {
-      throw new NotFoundException('No cities found');
-    }
-
-    return {
-      data,
-      totalCount,
-      totalPages,
-      currentPage,
-    };
-  }
-
   async delete(id: string): Promise<CityDocument> {
     return this.cityModel.findByIdAndDelete(id).exec();
+  }
+
+  async searchPaginated(options: PaginationOptions, filters: CityFilterDto): Promise<PaginationResult<CityDocument[]>> {
+    return paginateSearch<CityDocument>(
+      this.cityModel,
+      options,
+      (queryBuilder, filters) => {
+        if (filters?.name) {
+          queryBuilder.where('name').regex(new RegExp(filters.name, 'i'));
+        }
+        if (filters?.countryCode) {
+          queryBuilder.where('countryCode').equals(filters.countryCode);
+        }
+        return queryBuilder;
+      },
+      filters
+    );
   }
 
   async createWithDependencies(createCityDto: CreateCityDto): Promise<CityDocument> {
