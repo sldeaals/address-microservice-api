0dbe2da8db9a79bbc2ab043cee14e49002b5b0db
sldeaals <danielariasaguero@gmail.com>
2024-04-23 17:46:17 -0400

    update: api response dependencies after relocation

--- src/utils/api.response.util.ts
+++ src/utils/api.response.util.ts
@@ -1,7 +1,7 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, HttpStatus } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { ApiResponse } from '../common/api-response.interface';
+import { ApiResponse } from './api.response.interface.util';
 
 @Injectable()
 export class ApiResponseUtil<T> implements NestInterceptor<T, ApiResponse<T>> {
