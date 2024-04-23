fbe3061ab7eb926e6a2f27ce29da8b283c6b510b
sldeaals <danielariasaguero@gmail.com>
2024-04-23 17:51:10 -0400

    update: add roles guard to states controller

--- src/state/state.controller.ts
+++ src/state/state.controller.ts
@@ -1,16 +1,37 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, NotFoundException, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  NotFoundException,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { StateService } from './state.service';
 import { CreateStateDto, UpdateStateDto } from './state.dto';
 import { StateDocument } from './state.entity';
 import { ApiTags } from '@nestjs/swagger';
 import { PaginationOptions, PaginationResult } from '../utils/pagination.util';
+import { UserRole } from '../auth/auth.types';
+import { RolesGuard } from '../auth/auth.roles.guard';
+import { Roles } from '../utils/roles.decorator.util';
 
 @ApiTags('States')
+@UseGuards(RolesGuard)
 @Controller('state')
 export class StateController {
   constructor(private readonly stateService: StateService) {}
 
   @Get()
+  @Roles(
+    UserRole.SUPER_ADMIN,
+    UserRole.ADMIN,
+    UserRole.TECH_SUPPORT,
+    UserRole.CUSTOMER,
+  )
   async findAll(
     @Query() query: PaginationOptions,
     @Query('name') name: string,
@@ -21,6 +42,12 @@ export class StateController {
   }
 
   @Get(':id')
+  @Roles(
+    UserRole.SUPER_ADMIN,
+    UserRole.ADMIN,
+    UserRole.TECH_SUPPORT,
+    UserRole.CUSTOMER,
+  )
   async findById(@Param('id') id: string): Promise<StateDocument> {
     const state = await this.stateService.findById(id);
     if (!state) {
@@ -30,12 +57,17 @@ export class StateController {
   }
 
   @Post()
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
   async create(@Body() createStateDto: CreateStateDto): Promise<StateDocument> {
     return this.stateService.create(createStateDto);
   }
 
   @Put(':id')
-  async update(@Param('id') id: string, @Body() updateStateDto: UpdateStateDto): Promise<StateDocument> {
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.TECH_SUPPORT)
+  async update(
+    @Param('id') id: string,
+    @Body() updateStateDto: UpdateStateDto,
+  ): Promise<StateDocument> {
     const existingState = await this.stateService.findById(id);
     if (!existingState) {
       throw new NotFoundException('State not found');
@@ -44,6 +76,7 @@ export class StateController {
   }
 
   @Delete(':id')
+  @Roles(UserRole.SUPER_ADMIN)
   async delete(@Param('id') id: string): Promise<StateDocument> {
     const existingState = await this.stateService.findById(id);
     if (!existingState) {
