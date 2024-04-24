545fd66a6d204ee820beb0906343667c655c515e
sldeaals <danielariasaguero@gmail.com>
2024-04-24 13:25:21 -0400

    feat: create rateLimitMiddleware

---
+++ src/utils/rate-limit.middleware.ts
@@ -0,0 +1,12 @@
+import rateLimit from 'express-rate-limit';
+
+const RATE_LIMIT_WINDOWMS: number =
+  parseInt(process.env.RATE_LIMIT_WINDOWMS) || 15;
+const RATE_LIMIT_MAX_REQUESTS: number =
+  parseInt(process.env.RATE_LIMIT_MAX_REQUESTS) || 100;
+
+export const rateLimitMiddleware = rateLimit({
+  windowMs: RATE_LIMIT_WINDOWMS * 60 * 1000,
+  max: RATE_LIMIT_MAX_REQUESTS,
+  message: 'Too many requests from this IP, please try again later',
+});
