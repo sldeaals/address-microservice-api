defdef9489f6d968f38964fb9ca4a44e7510b38c
sldeaals <danielariasaguero@gmail.com>
2024-04-24 12:27:15 -0400

    feat: enforce HTTPS to encrypt data transmission

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import fs from 'fs';
+import https from 'https';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ExceptionsFilter } from './utils/exception.filter.util';
 import { ApiResponseUtil } from './utils/api.response.util';
+import { Environment } from './utils/types.util';
 import { RedisService } from './redis/redis.service';
 import { RedisCacheInterceptor } from './redis/redis.interceptor';
 import { ClusterService } from './cluster/cluster.service';
@@ -19,6 +22,7 @@ function useSwagger(app: INestApplication) {
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  const env = process.env.ENVIRONMENT;
 
   useSwagger(app);
   app.useGlobalFilters(new ExceptionsFilter());
@@ -27,6 +31,17 @@ async function bootstrap(): Promise<void> {
     new ApiResponseUtil()
   );
 
-  await app.listen(process.env.PORT);
+  if (env === Environment.PRODUCTION) {
+    const httpsOptions = {
+      key: fs.readFileSync('path/to/private-key.pem'),
+      cert: fs.readFileSync('path/to/certificate.pem'),
+    };
+    const httpsServer = https.createServer(httpsOptions, app.getHttpAdapter().getInstance());
+    httpsServer.listen(process.env.PORT, () => {
+      Logger.log(`Application is running on: ${process.env.PORT}`);
+    });
+  } else {
+    await app.listen(process.env.PORT);
+  }
 }
 ClusterService.clusterize(bootstrap);
